202c40dbe2fe370cbc4bbf62f744766384fbf201
ugo-reg <ugochi209@gmail.com>
2025-05-19 16:10:18 +0100

    added js for weather and changed my css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-function currentDate() {
-  let now = new Date();
-  let date = now.getDate();
-  let hours = now.getHours();
-  let minutes = now.getMinutes();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  let day = days[now.getDay()];
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = searchInputElement.value;
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
 
   if (minutes < 10) {
     minutes = `0${minutes}`;
@@ -14,16 +18,38 @@ function currentDate() {
     hours = `0${hours}`;
   }
 
-  let dateElement = document.querySelector(".date");
-  dateElement.innerHTML = `${day} ${hours}: ${minutes}`;
-}
-currentDate();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-function change(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search");
-  let h1 = document.querySelector("h1");
-  h1.innerHTML = `${searchInput.value}`;
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
 }
-let form = document.querySelector("#city");
-city.addEventListener("submit", change);
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+let currentDateELement = document.querySelector("#current-date");
+let currentDate = new Date();
+
+currentDateELement.innerHTML = formatDate(currentDate);
+
+function displayTemperature(response) {
+  console.log(response.data.temperature.current);
+  let temperatureElement = document.querySelector(".current-temperature-value");
+
+  let temperature = Math.round(response.data.temperature.current);
+  temperatureElement.innerHTML = `${temperature}`;
+}
+
+let city = "paris";
+let apiKey = "3ee9046f22cb8dtcf3aa949o097a3347";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+axios.get(apiUrl).then(displayTemperature);
